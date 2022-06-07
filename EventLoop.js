929cec2cd6df106e837ee0a45cf084f46bfcda79
Mohammad Zubair <mohammadzubair@Mohammads-MacBook-Air.local>
2022-06-07 09:47:11 +0530

    changes

---
+++ EventLoop.js
@@ -0,0 +1,15 @@
+console.log("first")
+setTimeout(() => {
+    console.log("Wait for 1 sec")
+}, 3000)
+setTimeout(() => {
+    console.log("Wait for 1 sec")
+}, 1000)
+setTimeout(() => {
+    console.log("Wait for 1 sec")
+}, 4000)
+setTimeout(() => {
+    console.log("Wait for 1 sec")
+}, 0)
+console.log("second")
+
---
+++ constructor.js
@@ -0,0 +1,27 @@
+//initialization of object
+let obj = {};
+//initialization of array
+let arr = []
+let arr1 = new Array()
+
+let obj1 = new Object();
+console.log(obj1)
+console.log(obj)
+
+//obj["name"] = "x";
+obj.name = "";
+
+function Animal(numLegs, havingHorns, sound, canFly) {
+    this.numLegs = numLegs;
+    this.havingHorns = havingHorns;
+    this.sound = sound;
+    this.canFly = canFly;
+}
+
+let dog = new Animal(4, false, "wuff", false)
+console.log(anim1)
+
+
+
+
+
